8120e0639896a303e874acdab38b7f5353788112
Ken Deeley <kdeeley@mathworks.com>
2025-06-11 16:41:07 +0100

    Testing without explicitly using coder

--- buildfile.m
+++ buildfile.m
@@ -8,7 +8,7 @@ function plan = buildfile()
 plan = buildplan( localfunctions() );
 
 % Set the archive task to run by default.
-plan.DefaultTasks = "generateCode";
+plan.DefaultTasks = "archive";
 
 % Add a test task to run the unit tests for the project. Generate and save
 % a coverage report.
